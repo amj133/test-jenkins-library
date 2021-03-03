a186c05472cbb586df3ea4d5bb1dc14464b3de55
ajeffery <Andrew.Jeffery@nrel.gov>
2021-03-03 16:49:45 -0500

    test

--- vars/test.groovy
+++ vars/test.groovy
@@ -2,4 +2,10 @@
 
 def call(Map args) {
   println("Open the pod bay doors Hal...");
+
+  env.TEST_VAR = "I can't do the Dave";
+
+  println("Hal...open the pod bay doors...");
+
+  println(env.TEST_VAR);
 }
